f5a8b6178c80c2dd0fdeeef4e03f5e01a27befe3
Lolors <rolilin8@gmail.com>
2026-07-22 22:24:49 +0900

    Set order quantity and unit columns to small width

--- components/editors.py
+++ components/editors.py
@@ -14,8 +14,8 @@ def order_editor(dataframe, *, key: str, dynamic: bool = True):
         column_config={
             '_id': None,
             '제품명': st.column_config.TextColumn('제품명', required=True),
-            '수량': st.column_config.NumberColumn('수량', min_value=0.0, step=1.0),
-            '단위': st.column_config.TextColumn('단위'),
+            '수량': st.column_config.NumberColumn('수량', min_value=0.0, step=1.0, width='small'),
+            '단위': st.column_config.TextColumn('단위', width='small'),
         },
     )
 
