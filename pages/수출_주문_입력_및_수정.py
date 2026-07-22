91ffebc818b048af183879d0e2b1fa675c3d2a4f
Lolors <rolilin8@gmail.com>
2026-07-22 23:17:34 +0900

    주문 입력 기본 정보 5열 레이아웃 조정

--- pages/수출_주문_입력_및_수정.py
+++ pages/수출_주문_입력_및_수정.py
@@ -14,6 +14,24 @@ from utils.numbering import next_export_no
 st.title('주문 입력')
 st.caption('현재 진행 건은 주문목록을 등록하고, 과거 수출 건은 주문목록을 실출고 제품으로도 자동 저장합니다.')
 
+st.markdown(
+    '''
+    <style>
+    div[data-testid="stVerticalBlock"] div[data-testid="stVerticalBlock"]:has(#new-case-basic-info-anchor) {
+        width: 60vw;
+        max-width: 60vw;
+    }
+    @media (max-width: 900px) {
+        div[data-testid="stVerticalBlock"] div[data-testid="stVerticalBlock"]:has(#new-case-basic-info-anchor) {
+            width: 100%;
+            max-width: 100%;
+        }
+    }
+    </style>
+    ''',
+    unsafe_allow_html=True,
+)
+
 st.markdown('#### 수출 주문 등록')
 case_type_label = st.radio(
     '등록 유형',
@@ -22,12 +40,6 @@ case_type_label = st.radio(
     key='new_case_type',
 )
 is_historical = case_type_label == '과거 수출 건'
-
-c1, c2 = st.columns(2)
-country = c1.text_input('국가 *', key='new_country')
-buyer = c2.text_input('바이어 (선택)', key='new_buyer')
-transport = c1.selectbox('운송방식', TRANSPORT_MODES, key='new_transport')
-note = c2.text_input('비고', key='new_note')
 
 historical_date = None
 if is_historical:
@@ -41,7 +53,14 @@ if is_historical:
 else:
     export_no_preview = next_export_no('EXP')
 
-st.text_input('수출번호', value=export_no_preview, disabled=True, key='new_export_no')
+with st.container():
+    st.markdown('<span id="new-case-basic-info-anchor"></span>', unsafe_allow_html=True)
+    export_no_col, country_col, buyer_col, transport_col, note_col = st.columns([2, 2, 2, 1, 3])
+    export_no_col.text_input('수출번호', value=export_no_preview, disabled=True, key='new_export_no')
+    country = country_col.text_input('국가 *', key='new_country')
+    buyer = buyer_col.text_input('바이어 (선택)', key='new_buyer')
+    transport = transport_col.selectbox('운송방식', TRANSPORT_MODES, key='new_transport')
+    note = note_col.text_input('비고', key='new_note')
 
 st.markdown('#### 주문 목록' if not is_historical else '#### 실출고 제품 목록')
 if is_historical:
