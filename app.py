d5ed69eba03bb599f786e9d6ccc6e14b55a04f48
Lolors <rolilin8@gmail.com>
2026-07-22 20:50:06 +0900

    Add separate order search menu

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ from config import APP_ICON, APP_LAYOUT, APP_TITLE
 PAGES = {
     '': [
         st.Page('pages/오버뷰.py', title='오버뷰', icon='📊', default=True),
-        st.Page('pages/수출_주문_입력_및_수정.py', title='수출 주문 입력 및 수정', icon='📝'),
+        st.Page('pages/수출_주문_입력_및_수정.py', title='주문 입력', icon='📝'),
+        st.Page('pages/주문_검색_및_수정.py', title='주문 검색 및 수정', icon='🔎'),
         st.Page('pages/실출고_입력.py', title='실출고 입력', icon='📦'),
         st.Page('pages/박스_패킹.py', title='박스 패킹', icon='📦'),
         st.Page('pages/국내배송.py', title='국내배송', icon='🚚'),
