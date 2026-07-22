940317c31a342f0719230cd5da69c58baaa02153
Lolors <rolilin8@gmail.com>
2026-07-22 20:23:32 +0900

    refactor: centralize application constants

---
+++ config.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+APP_TITLE = '수출관리'
+APP_ICON = '🌏'
+APP_LAYOUT = 'wide'
+
+STAGES = [
+    '주문 접수',
+    '제품 준비',
+    '실출고 입력',
+    '패킹',
+    '국내배송',
+    '선적 준비',
+    '선적 완료',
+    '완료',
+    '취소',
+]
+
+TRANSPORT_MODES = ['AIR', 'SEA', 'HAND']
+DOMESTIC_METHODS = ['로젠택배', '퀵배송']
+
+COMPANY_NAME = '주식회사 노투스팜'
+SYSTEM_NAME = '수출관리 시스템'
