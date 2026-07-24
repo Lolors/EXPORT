935586447384ef1cf9a2ce35eefb77d38cceac73
Lolors <rolilin8@gmail.com>
2026-07-24 15:28:41 +0900

    Use unified order search and historical edit page

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ PAGES = {
     '': [
         st.Page('pages/오버뷰.py', title='오버뷰', icon='📊', default=True),
         st.Page('pages/수출_주문_입력_및_수정.py', title='주문 입력', icon='📝'),
-        st.Page('pages/주문_검색_및_수정.py', title='주문 검색 및 수정', icon='🔎'),
+        st.Page('pages/주문_검색_및_수정_v2.py', title='주문 검색 및 수정', icon='🔎'),
         st.Page('pages/실출고_입력.py', title='수출대기 입고', icon='📦'),
         st.Page('pages/박스_패킹.py', title='박스 패킹', icon='📦'),
         st.Page('pages/국내배송.py', title='국내배송', icon='🚚'),
