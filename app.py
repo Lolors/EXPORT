ca1437e47e09b767021f3000bcde8697bd7111cc
Lolors <rolilin8@gmail.com>
2026-07-25 23:50:31 +0900

    구조: 앱 메뉴를 views 화면 경로로 전환

--- app.py
+++ app.py
@@ -22,15 +22,15 @@ from services import (
 
 PAGES = {
     '': [
-        st.Page('pages/오버뷰.py', title='대시보드', icon='📊', default=True),
-        st.Page('pages/수출_주문_입력_및_수정.py', title='주문 입력', icon='📝'),
-        st.Page('pages/주문_검색_및_수정_v2.py', title='주문 검색 및 수정', icon='🔎'),
-        st.Page('pages/실출고_입력.py', title='수출대기 입고', icon='📦'),
-        st.Page('pages/박스_패킹.py', title='박스 패킹', icon='📦'),
-        st.Page('pages/국내배송.py', title='국내배송', icon='🚚'),
-        st.Page('pages/공유문서.py', title='공유용 자료', icon='📄'),
-        st.Page('pages/기간별_통계.py', title='기간별 통계', icon='📈'),
-        st.Page('pages/내_폴더.py', title='내 폴더', icon='📁'),
+        st.Page('views/오버뷰.py', title='대시보드', icon='📊', default=True),
+        st.Page('views/수출_주문_입력_및_수정.py', title='주문 입력', icon='📝'),
+        st.Page('views/주문_검색_및_수정_v2.py', title='주문 검색 및 수정', icon='🔎'),
+        st.Page('views/실출고_입력.py', title='수출대기 입고', icon='📦'),
+        st.Page('views/박스_패킹.py', title='박스 패킹', icon='📦'),
+        st.Page('views/국내배송.py', title='국내배송', icon='🚚'),
+        st.Page('views/공유문서.py', title='공유용 자료', icon='📄'),
+        st.Page('views/기간별_통계.py', title='기간별 통계', icon='📈'),
+        st.Page('views/내_폴더.py', title='내 폴더', icon='📁'),
     ]
 }
 
