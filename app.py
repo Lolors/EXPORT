cb013527b0c47d03b259133aa9365bb932aa5918
Lolors <rolilin8@gmail.com>
2026-07-25 17:46:44 +0900

    박스 패킹 v3 화면 연결

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ PAGES = {
         st.Page('pages/수출_주문_입력_및_수정.py', title='주문 입력', icon='📝'),
         st.Page('pages/주문_검색_및_수정_v2.py', title='주문 검색 및 수정', icon='🔎'),
         st.Page('pages/실출고_입력_v2.py', title='수출대기 입고', icon='📦'),
-        st.Page('pages/박스_패킹_v2.py', title='박스 패킹', icon='📦'),
+        st.Page('pages/박스_패킹_v3.py', title='박스 패킹', icon='📦'),
         st.Page('pages/국내배송.py', title='국내배송', icon='🚚'),
         st.Page('pages/공유문서_v2.py', title='공유용 자료', icon='📄'),
         st.Page('pages/기간별_통계.py', title='기간별 통계', icon='📈'),
